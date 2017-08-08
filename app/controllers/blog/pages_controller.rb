2a7adacb72292e38718d85e057cd391448b81be3
Namith <Namith@Sunils-MacBook-Pro.local>
2017-08-08 12:27:06 -0500

    first commit

---
+++ app/controllers/blog/pages_controller.rb
@@ -0,0 +1,10 @@
+module Blog
+  class PagesController < BlogController
+    def about
+    end
+
+    def contact
+    end
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+
+
+  root to: 'blog/posts#index'
+
+
+  namespace :author do
+    resources :posts
+  end
+
+
+  scope module: 'blog' do
+    get 'about' => 'pages#about', as: :about
+    get 'contact' => 'pages#contact', as: :contact
+    get 'posts' => 'posts#index', as: :posts
+    get 'posts/:id' => 'posts#show', as: :post
+  end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
